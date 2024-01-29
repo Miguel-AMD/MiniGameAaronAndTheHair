f0f5be6e13b2787c25a59c5e5611c226ca8b5ec2
reboot <edomie@protonmail.com>
2024-01-29 15:12:33 +0000

    KEYBOARD DIRECTION INPUT works

--- Codigo/Script.js
+++ Codigo/Script.js
@@ -1,52 +1,68 @@
 window.onload = function(){
 const gameArena = document.getElementById('gameArena');
 const cat = document.getElementById('cat');
-const downButton = document.getElementById('downButton');
-const upButton = document.getElementById('upButton');
-const leftButton = document.getElementById('leftButton');
-const rightButton = document.getElementById('rightButton');
 
 
-//KEYPRESSES
-document.addEventListener("keydown", (e)) {
-    console.log(e);
-}
-
-
+//KEYBOARD INPUT
+document.addEventListener("keydown", (e) => {
+switch (e.key) {
+    case "ArrowDown":
+    case "s":
+        moveDown();
+        break
+    case "ArrowUp":
+    case "w":
+        moveUp();
+        break
+    case "ArrowLeft":
+    case "a":
+        moveLeft();
+        break
+    case "ArrowRight":
+    case "d":
+        moveRight();
+        break
+} 
+});
 
 //SETS COORDINATES
 var yPos= 0;
 var xPos=0;
+
 //MOVES DOWN
-downButton.onclick = (moveDown)
 function moveDown() {
     if (yPos < 700 && yPos >= 0) {
     yPos += 100;
     cat.style.top = yPos+"px";
     }
     }
+
 //MOVES UP
-upButton.onclick = (moveUp) 
 function moveUp() {
     if (yPos < 800 && yPos >= 100) {
     yPos -= 100;
     cat.style.top = yPos+"px";
     }
     }
+
+
 //MOVES RIGHT
-rightButton.onclick = (moveRight) 
 function moveRight() {
     if (xPos < 700 && xPos >= 0) {
     xPos += 100;
     cat.style.left = xPos+"px";
     }
     }
+
+
 //MOVES LEFT
-leftButton.onclick = (moveLeft) 
 function moveLeft() {
     if (xPos >= 100 && xPos <800) {
     xPos -= 100;
     cat.style.left = xPos+"px";
     }
     }
+
+
+//END OF CODE
 };
