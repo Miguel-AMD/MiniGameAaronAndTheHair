d614ac4d5e9a85106032adaee0262b0f324a1015
reboot <edomie@protonmail.com>
2024-01-29 14:14:29 +0000

    directional input with buttons

--- Codigo/Script.js
+++ Codigo/Script.js
@@ -1,22 +1,52 @@
 window.onload = function(){
 const gameArena = document.getElementById('gameArena');
- const cat = document.getElementById('cat');
+const cat = document.getElementById('cat');
 const downButton = document.getElementById('downButton');
+const upButton = document.getElementById('upButton');
+const leftButton = document.getElementById('leftButton');
+const rightButton = document.getElementById('rightButton');
+
+
+//KEYPRESSES
+document.addEventListener("keydown", (e)) {
+    console.log(e);
+}
 
 
 
-downButton.onclick = (yMove)
-onkeydown 
+//SETS COORDINATES
 var yPos= 0;
 var xPos=0;
-function yMove() {
-    yPos += 100
+//MOVES DOWN
+downButton.onclick = (moveDown)
+function moveDown() {
+    if (yPos < 700 && yPos >= 0) {
+    yPos += 100;
     cat.style.top = yPos+"px";
-}
-
-//only activate downKey if the top side is between Y positions 0 and containersize-height (700 in this case)
-if yPos < 700 || yPos > 0 {
-
-}
-
+    }
+    }
+//MOVES UP
+upButton.onclick = (moveUp) 
+function moveUp() {
+    if (yPos < 800 && yPos >= 100) {
+    yPos -= 100;
+    cat.style.top = yPos+"px";
+    }
+    }
+//MOVES RIGHT
+rightButton.onclick = (moveRight) 
+function moveRight() {
+    if (xPos < 700 && xPos >= 0) {
+    xPos += 100;
+    cat.style.left = xPos+"px";
+    }
+    }
+//MOVES LEFT
+leftButton.onclick = (moveLeft) 
+function moveLeft() {
+    if (xPos >= 100 && xPos <800) {
+    xPos -= 100;
+    cat.style.left = xPos+"px";
+    }
+    }
 };
