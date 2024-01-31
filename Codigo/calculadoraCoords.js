430ef49469467a056a942dc1f454ee062e1a9bd8
Miguel <mianmodi97@gmail.com>
2024-01-31 09:31:34 +0000

    Coordenadas Personaje

--- Codigo/calculadoraCoords.js
+++ Codigo/calculadoraCoords.js
@@ -7,15 +7,24 @@ function coordenadasMapa(){
 function posicionPersonaje(){
     let C = coordenadasMapa();
     let A = document.getElementById('Aaron');
+    A.style.left = (C[0]-5)+'px'; //No se porque el menos 5 pero hay que ponerlo
+    A.style.top = (C[1]+11)+'px';
+}
+
+//posicionPersonaje();
+
+function posicionCoordenadaMapa(x,y){
+    let C = coordenadasMapa();
+    let newX = (C[0]-5)+x*45;
+    let newY = (C[1]+11)+y*45;
+    return [newX,newY];
+}
+
+function MuevePersonaje(x,y,IdPersonaje){
+    let C = posicionCoordenadaMapa(x,y);
+    let A = document.getElementById(IdPersonaje);
     A.style.left = (C[0])+'px'; //No se porque el menos 5 pero hay que ponerlo
     A.style.top = (C[1])+'px';
 }
 
-posicionPersonaje();
-
-function posicionCoordenadaMapa(x,y){
-    let C = coordenadasMapa();
-    let newX = C[0]-5+x*45;
-    let newY = C[1]-5+y*45;
-    return [newX,newY];
-}
+MuevePersonaje(4,4,'Liebre')
